f30644885eb9270972db72c748d5c3684d3fea32
Maximiliano Gutierrez <hmgutierrezfernandez@gmail.com>
2023-02-24 20:43:24 +0100

    Implement Server and database

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import "./loadEnvironment.js";
+import createDebug from "debug";
+import startServer from "./server/startServer.js";
+import connectDataBase from "./database/connectDatabase.js";
+import mongoose from "mongoose";
+import chalk from "chalk";
+
+export const debug = createDebug("robots:*");
+
+const port = process.env.PORT ?? 4000;
+const mongoDdUrl = process.env.MONGODB_CONNECTION_URL;
+
+mongoose.set("toJSON", {
+  virtuals: true,
+  transform(doc, ret) {
+    delete ret._id;
+    delete ret.__v;
+  },
+});
+
+try {
+  await connectDataBase(mongoDdUrl!);
+  debug(chalk.green("Connected to data base"));
+  console.log("This is connected");
+
+  await startServer(+port);
+  debug(chalk.green(`Server listening on port ${port}`));
+  console.log("Working in port 4000");
+} catch (error) {
+  debug(error.message);
+}
