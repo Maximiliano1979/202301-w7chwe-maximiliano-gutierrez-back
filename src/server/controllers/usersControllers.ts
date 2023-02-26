b6c3ae7381fd4aedf3a1bc23de922d9d401db764
Maximiliano Gutierrez <hmgutierrezfernandez@gmail.com>
2023-02-26 17:19:52 +0100

    Update usersControllers and some minor fixes and package.json and tsconfig

--- src/server/controllers/usersControllers.ts
+++ src/server/controllers/usersControllers.ts
@@ -1,5 +1,7 @@
-import debug from "debug";
+import bcryptjs from "bcrypt";
 import { type NextFunction, type Request, type Response } from "express";
+import { validationResult } from "express-validator";
+import { CustomError } from "../../CustomError/CustomError.js";
 import User from "../../database/models/User.js";
 import {
   UserCredentials,
@@ -22,47 +24,136 @@ export const registerUser = async (
   res: Response,
   next: NextFunction
 ) => {
-  const { email, avatar, name, password, username } = req.body;
+  try {
+    const { email, avatar, name, password, username } = req.body;
 
-  const newUser = await User.create({
-    name,
-    username,
-    password,
-    email,
-    avatar,
-  });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors);
+    }
 
-  res.status(201).json(newUser);
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    const newUser = await User.create({
+      name,
+      username,
+      password: hashedPassword,
+      email,
+      avatar,
+    });
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    const customError = new CustomError(
+      (error as Error).message,
+      500,
+      "Couldn't create the user"
+    );
+
+    next(customError);
+  }
 };
 
-export const updateUserFriend = async (
+export const addUserFriend = async (
   req: Request<Record<string, unknown>, Record<string, unknown>, UserStructure>,
   res: Response,
   next: NextFunction
 ) => {
-  const id = req.params;
-  const newFriend = "63fa0f396f1692cbe7496c57";
+  try {
+    const id = req.params;
+    const newFriend = "63fa0f396f1692cbe7496c57";
 
-  const updateUser = await User.updateOne(
-    { _id: id },
-    { $push: { friends: newFriend } }
-  );
+    const updateUser = await User.updateOne(
+      { _id: id },
+      { $push: { friends: newFriend } }
+    );
 
-  res.status(201).json({ updateUser });
+    res.status(201).json({ updateUser });
+  } catch (error) {
+    const customError = new CustomError(
+      (error as Error).message,
+      404,
+      "Couldn't add new friend"
+    );
+
+    next(customError);
+  }
 };
 
-export const updateUserFoes = async (
+export const deleteUserFriend = async (
   req: Request<Record<string, unknown>, Record<string, unknown>, UserStructure>,
   res: Response,
   next: NextFunction
 ) => {
-  const id = req.params;
-  const newFriend = "63fa0f396f1692cbe7496c57";
+  try {
+    const id = req.params;
+    const newFriend = "63fa0f396f1692cbe7496c57";
 
-  const updateUser = await User.updateOne(
-    { _id: id },
-    { $pull: { friends: newFriend } }
-  );
+    const updateUser = await User.updateOne(
+      { _id: id },
+      { $pull: { friends: newFriend } }
+    );
 
-  res.status(201).json({ updateUser });
+    res.status(201).json({ updateUser });
+  } catch (error) {
+    const customError = new CustomError(
+      (error as Error).message,
+      404,
+      "Couldn't delete friend"
+    );
+
+    next(customError);
+  }
+};
+
+export const addUserFoes = async (
+  req: Request<Record<string, unknown>, Record<string, unknown>, UserStructure>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const id = req.params;
+    const newFriend = "63fa0f396f1692cbe7496c57";
+
+    const updateUser = await User.updateOne(
+      { _id: id },
+      { $push: { friends: newFriend } }
+    );
+
+    res.status(201).json({ updateUser });
+  } catch (error) {
+    const customError = new CustomError(
+      (error as Error).message,
+      404,
+      "Couldn't add new foe"
+    );
+
+    next(customError);
+  }
+};
+
+export const deleteUserFoes = async (
+  req: Request<Record<string, unknown>, Record<string, unknown>, UserStructure>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const id = req.params;
+    const newFriend = "63fa0f396f1692cbe7496c57";
+
+    const updateUser = await User.updateOne(
+      { _id: id },
+      { $pull: { friends: newFriend } }
+    );
+
+    res.status(201).json({ updateUser });
+  } catch (error) {
+    const customError = new CustomError(
+      (error as Error).message,
+      404,
+      "Couldn't delete foe"
+    );
+
+    next(customError);
+  }
 };
